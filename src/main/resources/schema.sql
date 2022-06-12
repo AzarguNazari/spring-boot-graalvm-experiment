d0ddce3f58e375b96c09cdbeebabea14cc3f63cc
Hazar Nazari <hazar.nazari@kuehne-nagel.com>
2022-06-12 11:20:08 +0200

    refactor

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 -- drop table if exists Tutorial;
 
 
-CREATE TABLE Tutorial (id INTEGER NOT NULL AUTO_INCREMENT,
+CREATE TABLE Tutorial (id INTEGER NOT NULL,
                        title VARCHAR(255),
                    description varchar(500),
                        published varchar(5),
